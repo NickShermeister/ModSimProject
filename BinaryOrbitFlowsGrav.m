902d3bde5e5e12733b9e695a8eb7244ba1c1a2c1
Nicholas Sherman <nsherman@olin.edu>
2016-11-29 20:14:37 -0500

    Grav assist working

--- BinaryOrbitFlowsGrav.m
+++ BinaryOrbitFlowsGrav.m
@@ -8,6 +8,7 @@ function res = BinaryOrbitFlows(~, state)
 %Variables
 mA = .6897*1.98855e30;         %Mass of Keplar-16A (Solar Masses to kg)
 mB = .20255*1.98855e30;        %Mass of Keplar-16B (Solar Masses to kg)
+mS = 2*10^6;                   %Mass of spaceship
 rA = 451439.73*1000;           %Orbit Radius of Keplar-16A (km to m)
 rB = 157388.211*1000;          %Orbit Radius of Keplar-16B (km to m)
 a = 3.2912e7;                  %Distance between stars (.22AU to km)
@@ -22,17 +23,31 @@ xB = state(5);       %x position B (m)
 yB = state(6);       %y position B (m)
 vxB = state(7);      %x velocity B (m/s)
 vyB = state(8);      %y velocity B (m/s)
+xS = state(9);       %x position S (m)
+yS = state(10);      %y position S (m)
+vxS = state(11);     %x velocity S (m)
+vyS = state(12);     %y velosity S (m)
 
 %Directions
 PA = [xA, yA];
 PB = [xB, yB];
+PS = [xS, yS];
 R = PB - PA;
+RAS = PS - PA;
+RBS = PS - PB;
 r = norm(R);
+ras = norm(RAS);
+rbs = norm(RBS);
 Rhat = R / r;
+Rhatas = RAS / ras;
+Rhatbs = RBS / rbs;
 
 %Forces
 %Fg = G*(m1*m2/r^2)
 Fg = (G.*(mA.*mB./r.^2)) .* Rhat;        %Force of gravity(kgm/s^2)
+Fgsa = G*mS*mA/ras^2 .* Rhatas;     %Force of A on S
+Fgsb = G*mS*mB/rbs^2 .* Rhatbs;     %Force of B on S
+Fts = Fgsa + Fgsb;                       %Total force on S
 
 %'Flows'
 dxAdt = vxA;      %Rate of change of x position A
@@ -43,6 +58,11 @@ dxBdt = vxB;      %Rate of change of x position B
 dyBdt = vyB;      %Rate of change of y position B
 dvxBdt = -Fg(1)./mB; %Rate of change of x velocity B
 dvyBdt = -Fg(2)./mB; %Rate of change of y velocity B
+dxSdt = vxS;      %Rate of change of x position S
+dySdt = vyS;      %Rate of change of y position S
+dvxSdt = -Fts(1)./mS %Rate of change of x velocity S
+dvySdt = -Fts(2)./mS %Rate of change of y velocity S
 
-res = [dxAdt; dyAdt; dvxAdt; dvyAdt; dxBdt; dyBdt; dvxBdt; dvyBdt];
+
+res = [dxAdt; dyAdt; dvxAdt; dvyAdt; dxBdt; dyBdt; dvxBdt; dvyBdt; dxSdt; dySdt; dvxSdt; dvySdt];
 end
--- BinaryOrbitPlotGrav.m
+++ BinaryOrbitPlotGrav.m
@@ -1,12 +1,12 @@
 %Plotting A Binary Star System
 
-function res = BinaryOrbitPlot(~)
+function res = BinaryOrbitPlotGrav(~)
 clf;
 
-state = [0, 0, .879e4, .879e4, 0, 3.2912e10, -2.99e4, -2.99e4];    %Intial state of stars (m,m,m/s,m/s)
-tF = 20e6;                   %End time of simulation (s)
+state = [0, 0, .879e4, .879e4, 0, 3.2912e10, -2.99e4, -2.99e4, 50000000000, 50000000000, -110000, -90000];    %Intial state of stars (m,m,m/s,m/s)
+tF = 1e6;                   %End time of simulation (s)
 
-[T, S] = ode45(@BinaryOrbitFlows, [0,tF], state);
+[T, S] = ode45(@BinaryOrbitFlowsGrav, [0,tF], state);
 
 xA = S(:,1);         %x positions A
 yA = S(:,2);         %y positions A
@@ -16,12 +16,47 @@ xB = S(:,5);         %x positions B
 yB = S(:,6);         %y positions B
 vxB = S(:,7);        %x velocities B
 vyB = S(:,8);        %y velocities B
+xS = S(:,9);         %x positions S
+yS = S(:,10);         %y positions S
+vxS = S(:,11);        %x velocities S
+vyS = S(:,12);        %y velocities S
 
 hold on
-comet(xA, yA)
-comet(xB, yB)
-xlabel('x (m)')
-ylabel('y (m)')
-title('Binary Stars')
+% comet(xA, yA)
+% comet(xB, yB)
+% comet(xS, yS)
+ xlabel('x (m)')
+ ylabel('y (m)')
+ title('Binary Stars')
+ animate_func(T,S);
+
+    function animate_func(T,S)
+        
+        X1 = S(:,1);
+        Y1 = S(:,2);
+        X2 = S(:,5);
+        Y2 = S(:,6);
+        X3 = S(:,9);
+        Y3 = S(:,10);
+        minmax = [min([X1;X2;X3]), max([X1;X2;X3]), min([Y1;Y2;Y3]), max([Y1;Y2;Y3])];
+        for i=1:length(T)
+            clf;
+            hold on;
+            axis(minmax);
+            draw_func(X1(i), Y1(i), X2(i), Y2(i), X3(i), Y3(i));
+            drawnow;
+            if i < length(T)
+                dt = T(i+1) - T(i);
+                speedup = 10000000000;
+                pause(dt/speedup);
+            end
+        end
+        hold off
+    end
+    function draw_func(x1, y1, x2, y2, x3, y3)
+        plot(x1, y1, 'r.', 'MarkerSize', 50);
+        plot(x2, y2, 'b.', 'MarkerSize', 20);
+        plot(x3, y3, 'g.', 'MarkerSize', 10);
+    end
 
 end
